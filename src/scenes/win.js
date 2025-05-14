0f78e1a051a67bf09dce2152b129b645dde45abe
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2025-05-14 10:12:54 -0400

    Add max score on win screen

--- src/scenes/win.js
+++ src/scenes/win.js
@@ -1,13 +1,21 @@
 import k from "../engine.js";
 
-export default async function win({ score}) {
+export default async function win({ score }) {
   k.setBackground([40, 60, 40]);
   k.add([
     k.text(`You grabbed ${score} coins!!!`, {
       size: 56,
       width: k.width(),
     }),
-    k.pos(40, 120),
+    k.pos(50, 120),
+    k.layer("ui"),
+  ]);
+
+  let maxScore = k.getData("maxScore", 0);
+  k.add([
+    k.text(`Max Score: ${maxScore}`, { size: 18 }),
+    k.pos(50, 200),
+    k.layer("ui"),
   ]);
 
   k.onKeyPress(() => {
