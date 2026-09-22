173172e8bea1590c38d37ca7a52c3f0f7d8aa0f2
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2026-09-22 09:11:24 -0400

    Fix relative path

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import splash from "./scenes/splash.js";
 import win from "./scenes/win.js";
 
 k.loadSprite("bean", "sprites/bean.png");
-k.loadSprite("coin", "/sprites/coin.png");
-k.loadSprite("spike", "/sprites/spike.png");
-k.loadSprite("grass", "/sprites/grass.png");
-k.loadSprite("ghosty", "/sprites/ghosty.png");
-k.loadSprite("portal", "/sprites/portal.png");
+k.loadSprite("coin", "sprites/coin.png");
+k.loadSprite("spike", "sprites/spike.png");
+k.loadSprite("grass", "sprites/grass.png");
+k.loadSprite("ghosty", "sprites/ghosty.png");
+k.loadSprite("portal", "sprites/portal.png");
 
-k.loadMusic("background", "/sounds/background.mp3");
-k.loadSound("score", "/sounds/score.mp3");
-k.loadSound("portal", "/sounds/portal.mp3");
-k.loadSound("hit", "/sounds/hit.mp3");
-k.loadSound("bell", "/sounds/bell.mp3");
-k.loadSound("powerup", "/sounds/powerup.mp3");
+k.loadMusic("background", "sounds/background.mp3");
+k.loadSound("score", "sounds/score.mp3");
+k.loadSound("portal", "sounds/portal.mp3");
+k.loadSound("hit", "sounds/hit.mp3");
+k.loadSound("bell", "sounds/bell.mp3");
+k.loadSound("powerup", "sounds/powerup.mp3");
 
 k.scene("splash", splash);
 k.scene("game", game);
